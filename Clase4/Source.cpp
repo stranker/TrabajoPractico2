8aac8fc18d35bb8a17342b50bfcdd732bbf226d2
Unknown <dani_mrx93@hotmail.com>
2018-04-24 12:40:17 -0300

    V0.11

--- Clase4/Source.cpp
+++ Clase4/Source.cpp
@@ -6,13 +6,13 @@ int main(int argc, char* argv[]) {
 
 	SDL_Window *window = SDL_CreateWindow("Juego", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
 	SDL_Renderer *renderer;
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
+
 	if (window)
 	{
 		renderer = SDL_CreateRenderer(window, -1, 0);
+		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
 	}
-	SDL_Delay(3000);
 	return 0;
 }
