4dbffb228ef6fabeec5c25ce26a61b9f4c189a9f
Unknown <dani_mrx93@hotmail.com>
2018-04-25 10:03:17 -0300

    V0.12
    Add Image Lib

---
+++ Clase4/Game.cpp
@@ -0,0 +1,60 @@
+#include "Game.h"
+
+Game::Game()
+{
+}
+
+
+Game::~Game()
+{
+}
+
+void Game::init(const char* windowTitle, int xpos, int ypos, int width, int height, bool fullscreen)
+{	
+	int flag = 0;
+	if (fullscreen)
+		flag = SDL_WINDOW_FULLSCREEN;
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+	{
+		window = SDL_CreateWindow(windowTitle, xpos, ypos, width, height, flag);
+		if (window)
+			renderer = SDL_CreateRenderer(window, -1, 0);
+		if (renderer)
+		{
+			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		}
+		isRunning = true;
+
+	}
+	else
+		isRunning = false;
+}
+
+void Game::handleEvents()
+{
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	switch (event.type)
+	{
+		case SDL_QUIT:
+			isRunning = false;
+			break;
+		default:	
+		break;
+	}
+}
+void Game::update()
+{
+}
+void Game::render()
+{
+
+}
+void Game::clean()
+{
+
+}
+bool Game::running()
+{
+	return isRunning;
+}
---
+++ Clase4/Game.h
@@ -0,0 +1,25 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+
+class Game
+{
+private:
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	bool isRunning;
+public:
+	Game();
+	~Game();
+
+	void init(const char* windowTitle, int xpos, int ypos, int width, int height, bool fullscreen);
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+	bool running();
+};
+
+#endif
